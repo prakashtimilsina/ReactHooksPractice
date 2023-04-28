3b6f8df838224e102b53f061b2247013011e4c9a
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-04-28 14:02:45 -0500

    custom useFetch hook added

--- CustomReactHooks/custom-hooks/src/App.jsx
+++ CustomReactHooks/custom-hooks/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
 import "./App.css";
+import useFetch from "./useFetch";
 
 function App() {
+  const BASE_URL = "https://inshorts.deta.dev/news?category=science"
+  const { data: news, loading, error} = useFetch(BASE_URL);
   return (
     <>
       <div className="App">
-        <h1> Hello World </h1>
+        <h1> Custom React Hook (Data Fetching) </h1>
+        {loading && <h3>Loading...</h3> }
+        {error && <h3>Error..Something went wrong</h3>}
+       <div>
+        <pre>{JSON.stringify(news, undefined,2)}</pre>
+       </div>
       </div>
     </>
   );
